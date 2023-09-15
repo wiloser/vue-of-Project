5e18b9774b2b646ae2206801b775610b803d2bad
wiloser <1229054438@qq.com>
2023-09-15 22:53:43 +0800

    配置django后台

--- study_django/datas/urls.py
+++ study_django/datas/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('hello', views.hello, name='hello'),
+    path('login', views.login, name='login'),
 ]
--- study_django/datas/views.py
+++ study_django/datas/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse, JsonResponse
 
 def hello(request):
     data = {'context': 'fuck you', }
-    return JsonResponse(data)
+    return JsonResponse(data, safe=False)
+
+
+def login(request):
+    data = {''}
+    return JsonResponse(data, safe=False)
